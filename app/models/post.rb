54b8801d31289397d3ef9595f061cd25d5424605
souad988 <souadelmansouri88@gmail.com>
2023-09-23 10:39:52 +0100

    Add associations to models and add additional methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,20 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
+  has_many :likes
+  has_many :comments
+
+  attribute :title, :string
+  attribute :text, :text
+  attribute :comments_counter, :integer, default: 0
+  attribute :likes_counter, :integer, default: 0
+
+  after_save :update_user_posts_counter
+
+  def update_user_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
+
+  def recent_posts(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  has_many :posts, foreign_key: :author_id
+  has_many :comments, foreign_key: :author_id
+  has_many :likes, foreign_key: :author_id
+
+  private
+
+  def update_post_counter
+    update_column(:post_counter, posts.count)
+  end
+
+  def recent_posts(limit = 3)
+    posts.order(created_at: :desc).limit(limit)
+  end
 end
