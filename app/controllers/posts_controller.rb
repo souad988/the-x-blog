58da0578c96ba8897196b8b5fcf653b98474d6e9
souad988 <souadelmansouri88@gmail.com>
2023-10-02 14:43:10 +0100

    Fix n+1 fetch problem for post comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,10 @@ class PostsController < ApplicationController
   def index
     if params[:user_id].present?
       # List posts of a specific user
+      
       @user = User.find(params[:user_id])
-      @posts = @user.posts
+      #@posts = @user.posts
+      @posts = Post.includes(:author).where(author: params[:user_id])
     else
       # List all posts
       @posts = Post.all
